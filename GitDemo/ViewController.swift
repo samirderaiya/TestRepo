9bfa865311242f14f9ee60dd1666aebbbe54a0bf
SAMIR <samir@samirs-mac.local>
2018-10-02 10:26:49 -0700

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by SAMIR on 02/10/18.
+//  Copyright © 2018 SAMIR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
