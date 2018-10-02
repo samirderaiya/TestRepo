658b244e918ee554538def5504009c4c975ef4b1
SAMIR <samir@samirs-mac.local>
2018-10-02 11:11:00 -0700

    Commit From Harsh

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //HELLO COMMIT
         
-        
+        // This is commit from new git Heep Heep Hurray!!!!
         
         
         
